78e4ead702de989cd78f4b006bdf3f44c34932c3
Tomiigonzalezz00 <tomiigonzalezz16@gmail.com>
2022-10-26 04:35:46 -0300

    Punto 2 compraVenta

--- Tp Compra Venta/Resolucion.hs
+++ Tp Compra Venta/Resolucion.hs
@@ -1,19 +1,25 @@
+import Data.List(delete)
 import Data.ByteString (elemIndex)
+type Dinero = (Float,String)
+type Moneda = String 
+type Importe = Float
+
 data Persona = UnaPersona {
     nombre :: String,
-    cuenta:: [(Float,String)],
+    cuenta:: [Dinero],
     nivelDeSatisfaccion :: Int
 }  deriving (Show,Eq)
 
-data Producto = UnProducto {descripcion::String, 
-precio::Float, 
-moneda::String
+data Producto = UnProducto {
+descripcion::String, 
+precio::Importe, 
+moneda::Moneda
 } deriving (Show,Eq)  
 
 cacho:: Persona
 cacho = UnaPersona {
     nombre = "pedro",
-     cuenta = [(100, "peso"), (400, "real"), (1, "dolar")],
+     cuenta = [(100, "peso"), (1, "dolar"), (400, "real")],
      nivelDeSatisfaccion = 4
     } 
 
@@ -31,12 +37,17 @@ nombreM = snd
 
 
 
-cotizaciones :: [(Float,String)]
+cotizaciones :: [Dinero]
 cotizaciones = [(1, "peso"), (9, "dolar"), (4, "real"), (8,"euro")]
 
 --1) 
 --a)
-tipoCambio moneda = (fst.head.filter ((moneda==).nombreM)) cotizaciones
+tipoCambio :: String -> Float
+tipoCambio divisa = (importe.head.filter (\moneda -> nombreM moneda == divisa )) cotizaciones
+
+
+--Sin Expresion lamda: 
+--tipoCambio moneda = (fst.head.filter ((moneda==).nombreM)) cotizaciones
 
 --b) 
 convertirA :: String -> (Float, String) -> Float
@@ -47,15 +58,34 @@ convertirA obtengo (importe,tengo) = importe * tipoCambio tengo / tipoCambio obt
 tieneMoneda moneda persona = any ((moneda==).nombreM) (cuenta persona)
 
 
-
-cantidadDe :: String -> Persona -> [(Float, String)]
 cantidadDe moneda persona
-    | tieneMoneda moneda persona = filter ((moneda==).nombreM) (cuenta persona)
+    | tieneMoneda moneda persona = (importe.head.filter ((moneda==).nombreM)) (cuenta persona)
     | otherwise = 0
-
 
 --d) 
 pasarAPeso = convertirA "peso" 
 totalAhorro persona = sum (map (pasarAPeso) (cuenta persona))
 
 --Ejercicio 2 
+expresadoEn producto = moneda producto
+
+
+alfajor = UnProducto {
+    descripcion = "poderoso el chiquitin",
+    precio = 10,
+    moneda = "real"
+}  
+
+valorProducto = precio
+
+
+descuentoValorDelProducto producto (importe,moneda)= (importe - precio producto, moneda)
+
+
+tieneDivisaDeProducto producto persona = head (filter ((moneda producto==).nombreM) (cuenta persona))
+
+cantidadMoneda divisa = importe.head.filter (\moneda -> nombreM moneda == divisa)
+
+
+comprar producto persona = persona {cuenta = delete (cantidadMoneda (moneda producto) (cuenta persona),moneda producto) (cuenta persona ++ map (descuentoValorDelProducto producto) (filter ((moneda producto==).nombreM) (cuenta persona)))}
+
