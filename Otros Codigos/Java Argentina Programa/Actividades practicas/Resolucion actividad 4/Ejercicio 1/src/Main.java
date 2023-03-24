c9277d052246ddca99cddd502dfc7580a00e8d59
Tomiigonzalezz00 <tomiigonzalezz16@gmail.com>
2023-03-23 23:50:34 -0300

    Cambios
    Cambios

---
+++ Otros Codigos/Java Argentina Programa/Actividades practicas/Resolucion actividad 4/Ejercicio 1/src/Main.java
@@ -0,0 +1,15 @@
+import java.util.Arrays;
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        int [] vector = ingresoPorTeclado.registrar();
+        char modo = ingresoPorTeclado.comoOrdenar();
+        if (modo != 'e') {
+            int[] vectorOrd = ordenamientoDeVector.ordenar(vector, modo);
+            System.out.println(Arrays.toString(vectorOrd));
+        }
+        else
+            System.out.println("Ingresó una letra invalida");
+    }
+}
+
