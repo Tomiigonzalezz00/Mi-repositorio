9092719f0538190afbd2430afcdca3394d24bd90
Tomiigonzalezz00 <tomiigonzalezz16@gmail.com>
2023-04-21 15:14:42 -0300

    Creacion carpeta de comunicaciones y archivo de trabajo practico 2

---
+++ UTN FRD/Comunicaciones/Trabajos practicos/Trabajo_Practico_1.py
@@ -0,0 +1,15 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from scipy import signal 
+plt.style.use('seaborn')
+
+
+
+def fourier(x,n):
+    f = A*abs(sen(w0*t));
+
+T = np.pi;
+
+plt.plot([1, 2, 3, 4])
+plt.ylabel('some numbers')
+plt.show()
