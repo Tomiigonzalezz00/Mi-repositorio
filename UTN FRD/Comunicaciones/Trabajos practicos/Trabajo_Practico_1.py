a6d8d9213ad53921cd6d4ce33541cc22290801ce
Tomiigonzalezz00 <tomiigonzalezz16@gmail.com>
2023-04-21 19:57:25 -0300

    Creacion de serie de fourier

--- UTN FRD/Comunicaciones/Trabajos practicos/Trabajo_Practico_1.py
+++ UTN FRD/Comunicaciones/Trabajos practicos/Trabajo_Practico_1.py
@@ -1,15 +1,52 @@
 import numpy as np
 import matplotlib.pyplot as plt
-from scipy import signal 
+from scipy.integrate import quad 
 plt.style.use('seaborn')
 
 
+#Constantes
+pi = np.pi  
+seno = np.sin
+cos= np.cos
+#Periodo 
+T= 2*pi
+#Numero maximo de coeficientes
+maxcoeficientes=3
+#Variables 
+t = np.arange(0,T,0.001)
 
-def fourier(x,n):
-    f = A*abs(sen(w0*t));
+#Listas con coeficientes
+An = []
+Bn = []
 
-T = np.pi;
+def funcion(t):
+    return np.sin(t)
 
-plt.plot([1, 2, 3, 4])
-plt.ylabel('some numbers')
+#Componente continua
+An.append((1/T)*quad(funcion,-T/2,T/2)[0])
+Bn.append(0)
+
+    
+def coeficientes():
+    for n in range(1,maxcoeficientes+1):
+        def funcionA(t):
+            return funcion(t)*cos(2*pi*(1/T)*n)
+        def funcionB(t):
+            return funcion(t)*seno(2*pi*(1/T)*n)
+        An.append((1/T)*quad(funcionA,-T/2,T/2)[0])
+        Bn.append((1/T)*quad(funcionB,-T/2,T/2)[0])
+
+def sumatoria():
+    coeficientes()
+    sum = 0 
+    for n in range(1,maxcoeficientes+1):
+        sum += (An[n]*cos(2*pi*(1/T)*n*t) + Bn[n]*seno(2*pi*(1/T)*n*t))
+    return sum
+    
+def fourierTrigonometrica():
+    return An[0] + (2 * sumatoria())
+
+print(sumatoria())
+plt.plot(t,funcion(t))
+plt.plot(t,fourierTrigonometrica())
 plt.show()
